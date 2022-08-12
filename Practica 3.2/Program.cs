cf52f18baaed793bfc33a9781795121ba7398a6d
Fate <Fate@DESKTOP-QKB1521>
2022-08-12 09:55:02 +0200

    Practica 3.3

--- Practica 3.2/Program.cs
+++ Practica 3.2/Program.cs
@@ -6,7 +6,19 @@ namespace Practica_3._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese el numero a evaluar");
+            int Multiplo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el numero del limite");
+            int limite = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i <= limite; i++)
+            {
+                if(i % Multiplo == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
         }
     }
 }
