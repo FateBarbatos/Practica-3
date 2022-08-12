03d5d20206204ed898eb6da8b118138cd6e9b21b
Fate <Fate@DESKTOP-QKB1521>
2022-08-12 09:20:29 +0200

    Agregar archivos de proyecto.

---
+++ Practica 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Practica_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nota promedio");
+            double nota = double.Parse(Console.ReadLine());
+
+            if(nota >= 0 && nota <= 10)
+            {
+                if (nota >= 6)
+                {
+                    Console.WriteLine("Aprobado");
+                }
+                else if (nota >= 6)
+                {
+                    Console.WriteLine("Reposicion");
+                }
+                else
+                {
+                    Console.WriteLine("No aprobado");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nota invalida");
+            }
+            
+        }
+    }
+}
