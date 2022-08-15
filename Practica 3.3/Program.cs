c9b0f9e23e325c3104905920c200e67b3259f2e8
Fate <Fate@DESKTOP-QKB1521>
2022-08-15 11:23:03 +0200

    3.2

---
+++ Practica 3.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Practica_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool condicion = true;
+            int contador = 0;
+            
+            Console.WriteLine("Ingrese un numero limite");
+            int limite = int.Parse(Console.ReadLine());
+
+            while(condicion)
+            {
+                Console.WriteLine(contador);
+                if(contador <= limite)
+                {
+                    condicion = false;
+                }
+
+                contador += 1;
+            }
+        }
+    }
+}
